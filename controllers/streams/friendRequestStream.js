3aba7e460ceb9623931e4d6c4d968269db769655
asifistiaque <asifistiaque.ai@gmail.com>
2021-04-12 04:02:08 +0600

    asdasdsads

--- controllers/streams/friendRequestStream.js
+++ controllers/streams/friendRequestStream.js
@@ -1,25 +1,28 @@
 import { io } from '../../server.js';
+import Friend from '../../models/friendModel.js';
 
 const friendRequestStream = doc => {
 	console.log('friend req stream started');
 	if (doc.operationType == 'insert') {
+		console.log(doc);
 		io.of('/users')
 			.to(doc.fullDocument.receiver)
 			.emit('newFriendRequest', doc.fullDocument);
 	} else if (doc.operationType == 'delete') {
-		io.of('/users')
-			.to(doc.fullDocument.receiver)
-			.emit('canceledFriendRequest', doc.fullDocument);
+		console.log(doc);
+		// io.of('/users')
+		// 	.to(doc.fullDocument.receiver)
+		// 	.emit('canceledFriendRequest', doc.fullDocument);
 	} else if (doc.operationType == 'update') {
-		const status = doc.fullDocument.status;
-		if (status == 'friends') {
-			io.of('/users')
-				.to(doc.fullDocument.sender)
-				.emit('newFriend', doc.fullDocument);
-			io.of('/users')
-				.to(doc.fullDocument.receiver)
-				.emit('newFriend', doc.fullDocument);
-		}
+		console.log(doc);
+
+		Friend.findOne(doc.documentKey)
+			.then(res => {
+				console.log(res);
+				io.of('/users').to(doc.sender).emit('newFriend', doc.fullDocument);
+				io.of('/users').to(doc.receiver).emit('newFriend', doc.fullDocument);
+			})
+			.catch(e => console.log(e));
 	}
 };
 
