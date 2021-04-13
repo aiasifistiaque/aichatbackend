1a3a1241e40697c47f72f0a4dcabf52b724e8b07
asifistiaque <asifistiaque.ai@gmail.com>
2021-04-14 03:40:04 +0600

    adasdsdsdsd

--- routes/latestChatsRoute.js
+++ routes/latestChatsRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import LatestChat from '../models/latestChatModel.js';
+import { io } from '../server.js';
 
 const router = express.Router();
 
@@ -25,14 +26,28 @@ router.post('/', async (req, res) => {
 router.put('/changestatus', async (req, res) => {
 	const { user, friend } = req.body;
 
+	//console.log('i am here now', req.body);
+
 	const findReceiver = await LatestChat.findOne({
-		user: friend,
-		friend: user,
+		user: user,
+		friend: friend,
 	});
 
 	if (findReceiver) {
+		//console.log(findReceiver);
+		if (findReceiver.isSender) {
+			res.status(200).json({ changed: false, error: true });
+		}
+
 		findReceiver.status = 'seen';
-		await findReceiver.save();
+		const query = await findReceiver.save();
+		if (query) {
+			console.log(query);
+			io.of('/users')
+				.to(`activechats${query.user}`)
+				.emit('updateactivechats', query);
+		}
+
 		res.status(200).json({ changed: true, error: false });
 	} else {
 		res.status(404).json({ changed: false, error: true });
